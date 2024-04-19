5b27e8f9886e3e89e23487bbd88e134fe4834ac6
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-04-19 14:43:18 +0530

    Add files via upload

---
+++ components/main/Encryption.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+
+import { motion } from "framer-motion";
+import { slideInFromTop } from "@/utils/motion";
+import Image from "next/image";
+import { TimeLineData } from "@/utils/timeline"
+
+const Encryption = () => {
+  return (
+    <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
+      <div className="absolute w-auto h-auto top-0 z-[5]">
+        <motion.div
+          variants={slideInFromTop}
+          className="text-[40px] font-medium text-center text-gray-200"
+        >
+          Performance
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            {" "}
+            &{" "}
+          </span>
+          Security
+       </motion.div>
+      </div>
+
+  
+    
+
+
+
+
+
+      <div className="absolute z-[20] bottom-[10px] px-[5px]">
+        <div className="cursive text-[20px] font-medium text-center text-gray-300">
+          Secure your data with end-to-end encryption
+        </div>
+      </div>
+
+      <div className="w-full flex items-start justify-center absolute">
+        <video
+          loop
+          muted
+          autoPlay
+          playsInline
+          preload="false"
+          className="w-full h-auto"
+          src="/encryption.webm/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Encryption;
---
+++ utils/timeline.ts
@@ -0,0 +1,8 @@
+export const TimeLineData = [
+    { year: 2019, text: 'Started my journey as a B.Tech student at Punjab Engineering College (PEC).', },
+    { year: 2020, text: 'Started learning DSA and Web Development.', },
+    { year: 2022, text: 'Landed an Internship at Coding Ninjas for displaying proficiency in DSA.', },
+    { year: 2023, text: 'Landed a full time job as a Business Analyst.', },
+    { year: 2024, text: 'Building my portfolio one step at a time.', }
+    
+  ];
